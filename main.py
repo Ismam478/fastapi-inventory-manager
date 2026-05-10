cc9accc4e6983ac4d3be6af6c5a746559e5451e1
Ismam478 <negom4444@gmail.com>
2026-05-11 03:08:54 +0600

    Fetching Product by ID and handling 404 error if not found

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from src.database_model import Product, ItemInput, BaseM, User, UserInput, UserLoginInput
 from src.database import Session1, engine1
 import src.database_model as db_model
@@ -29,11 +29,13 @@ def welcome(db: Session = Depends(get_db)):
 app.include_router(users.router)
 app.include_router(products.router)
 
-@app.get("/products")
-def get_products(db: Session = Depends(get_db)):
-    db_products = db.query(Product).all()
-    return db_products
-
+@app.get("/products/{product_id}")
+def get_products(product_id: int, db: Session = Depends(get_db)):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+    if db_product:
+        return db_product
+    else:
+        raise HTTPException(status_code=404, detail="Product not found")
 
 
     
